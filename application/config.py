af1b394d3ce5ea717fbf8320e94e2f8aed903c66
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2018-06-28 12:31:04 +0200

    Create config.py

---
+++ application/config.py
@@ -0,0 +1,6 @@
+'''
+This file is configuration file for the flask application.
+'''
+SECRET_KEY="#fff" # secret key for signing sessions
+USERNAME='admin'       # change username here
+PASSWORD='test123'         # change password here
