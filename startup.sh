db7e29651e011076b5a3bdbc55dbfeceaad94b06
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2018-08-09 15:53:14 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,4 @@
- wget https://orientdb.com/download.php?file=orientdb-community-2.1.2.tar.gz
- 
+wget -O orientdb-community-2.2.20.tar.gz http://orientdb.com/download.php?file=orientdb-community-2.2.20.tar.gz&os=linux
+tar -zxvf orientdb-community-2.2.20.tar.gz
+sudo mv orientdb-community-2.2.20 /opt/orientdb
+
