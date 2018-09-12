45d0c71d8eb88de015181cc999bf0d2dee1d8f17
Lscheinman <40657768+Lscheinman@users.noreply.github.com>
2018-09-12 14:02:39 +0200

    Update OsintCrawler.py

--- application/services/OsintCrawler.py
+++ application/services/OsintCrawler.py
@@ -97,7 +97,6 @@ class Crawler():
         self.ActiveDriver = self.chromeDriver    
         self.chromeDriver.get(self.startURL)
         
-    
     def getText(self):
         text = self.ActiveDriver.find_elements_by_class_name('text')
         self.data['firstPagetext'] = []
